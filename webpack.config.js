aa2e4909c28712edf08d9ea13b4a2f6b7b0468fe
Rick Gallegos <duulketariakan@yahoo.com>
2017-10-12 20:54:09 -0700

    Redo file structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,18 @@ const path = require('path');
 // Constant with our paths
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
-  JS: path.resolve(__dirname, 'client'),
+  CLIENT: path.resolve(__dirname, 'src/client'),
+  SRC: path.resolve(__dirname, 'src')
 };
 
 // Webpack configuration
 module.exports = {
-  entry: path.join(paths.JS, 'app.js'),
+  entry: path.join(paths.CLIENT, 'app.js'),
   output: {
     path: paths.DIST,
     filename: 'app.bundle.js'
   },
+  devServer: {
+    contentBase: paths.SRC,
+  }
 };
