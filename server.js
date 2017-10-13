3b9e93ca53d7db1ff39dbaee492a96f9c61bba78
Rick Gallegos <duulketariakan@yahoo.com>
2017-10-13 01:40:48 -0700

    Add jest

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // this is where it all begins
 require('dotenv').config();
+
 import app from './server/index';
 
 const port = process.env.PORT || 7711;
 const host = process.env.HOST || '127.0.0.1';
 
 app.listen(port, () => {
-  console.log(`Listening at http:${host}:${port}`)
+  console.log(`Listening at http:${host}:${port}`);
 });
