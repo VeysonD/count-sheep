c0ace3cca8db9dc0ee128b49ae875358921b6edf
Rick Gallegos <duulketariakan@yahoo.com>
2017-10-26 22:59:24 -0700

    Files compile successfully with webpack

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,9 +1,5 @@
 import auth0 from 'auth0-js';
 
-// auth0 = 'chicken';
-// console.log('what is auth0:', auth0);
-// console.log('what is auth0:', JSON.stringify(auth0));
-
 export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'fdash4.auth0.com',
