711548a00d482dad94b5d1468fee5f95c9243df7
Rick Gallegos <duulketariakan@yahoo.com>
2017-10-29 23:39:39 -0700

    Remove Auth0 for now

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -14,8 +14,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const auth = new Auth();
-    auth.login();
+    // const auth = new Auth();
+    // auth.login();
   }
 
   testFunction() {
